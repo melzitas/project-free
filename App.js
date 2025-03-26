1e33160717de068ef1c85e0119554557e3ca16cf
mel <eufraziomelissa@gmail.com>
2025-03-26 10:49:16 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './components/style.css';
+
+
+function App() {
+  return (
+    <div className= "App">
+      <main>
+        <section id= "about">
+          <h1>OIIII! ME CHAMO MELISSA!</h1>
+          <img src="https://s4.static.brasilescola.uol.com.br/img/2019/09/panda.jpg"></img>
+          <p>Tenho 17 anos e gosto de comer sushi. Amo pandas pelo simpes fato de que eles sao fofos e idiotas.</p>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
